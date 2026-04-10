88feb2a24a6ee3d4f460ea8f1d321ad0441f9a8f
michael-myers-myke <zuluagamaikol1@gmail.com>
2026-04-10 17:40:41 -0500

    Cambios dashboard

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -3,7 +3,6 @@ import Registro from './pages/login/registro';
 import Login from './pages/login/login';
 import Dashboard from './pages/dashboard/dashboard';
 import Materias from './pages/materias/materias';
-import MateriasDeshabilitadas from './pages/materias/materiasDeshabilitadas';
 import Tareas from './pages/tareas/tareas';
 import TareasDeshbilitadas from './pages/tareas/tareasDeshabilitadas';
 import MiPerfil from "./pages/miPerfil/miPerfil";
@@ -15,7 +14,6 @@ function AppRouter() {
             <Route path="/login" element={<Login/>}></Route>
             <Route path="/dashboard" element={< Dashboard/>}></Route>
             <Route path="/materias" element={< Materias/>}></Route>
-            <Route path="/materiasDeshabilitadas" element={< MateriasDeshabilitadas/>}></Route>
             <Route path="/tareas" element={< Tareas/>}></Route>
             <Route path="/tareasDeshabilitadas" element={< TareasDeshbilitadas/>}></Route>
             <Route path="/perfil" element={< MiPerfil/>}></Route>
