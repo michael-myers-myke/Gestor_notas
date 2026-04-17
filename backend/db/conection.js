1153099d446765368c76ce805eb74b912263105c
michael-myers-myke <zuluagamaikol1@gmail.com>
2026-04-17 17:07:27 -0500

    Cambios despliegue

--- backend/db/conection.js
+++ backend/db/conection.js
@@ -1,10 +1,13 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
-dotenv.config(); //Siempre debo usar el .env para que se puedan cargar los parametros del archivo .ENV si no no se van a lerr
+dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 pool.connect()
@@ -12,5 +15,3 @@ pool.connect()
 .catch((error) => console.error("Error al conectar a la db: ", error));
 
 module.exports = pool;
-
-
